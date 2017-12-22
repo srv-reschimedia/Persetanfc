ac55222dfe3424f3b34880282e815abfe6f263e5
srv-reschimedia <reschimedia@gmail.com>
2017-12-22 21:53:51 +0700

    add tambah meta tag website

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 <html lang="en">
 <head>
   <!-- Theme Made By www.w3schools.com - No Copyright -->
-  <title>Bootstrap Theme Company Page</title>
+  <title>PERSETAN FC official | Persatuan Sepak Bola Tirisan</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="style/css/bootstrap.min.css">
